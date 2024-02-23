547b29e2f33200e9b664530a763e22c5cd06c974
Taip Gün <taip.guen@wambo.com>
2024-02-23 07:43:59 +0100

    Seeders & Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -12,11 +12,6 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -24,21 +19,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            "firstname" =>$this->faker->name,
+            "lastname" => $this->faker->name,
+            "sex" => "male",
+            "email" => $this->faker->email,
+            "phone" => $this->faker->phoneNumber(),
+            "username" => $this->faker->name(),
+            "password" => $this->faker->password(),
+            "birthdate" => $this->faker->date(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
